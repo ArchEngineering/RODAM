c59e0413882113087d922e7e1fc6edd6de40e511
ArchEngineering <157379445+ArchEngineering@users.noreply.github.com>
2024-01-23 15:06:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,74 @@
+// Billeder
+document.addEventListener('DOMContentLoaded', function () {
+  const sizeSelect = document.getElementById('size');
+  const facadeSelect = document.getElementById('facade');
+  const combinationImage = document.getElementById('combinationImage');
+
+  updateImageSource(1, 1);
+
+  sizeSelect.addEventListener('change', function () {
+    updateImageSource(parseInt(sizeSelect.value), parseInt(facadeSelect.value));
+  });
+
+  facadeSelect.addEventListener('change', function () {
+    updateImageSource(parseInt(sizeSelect.value), parseInt(facadeSelect.value));
+  });
+
+  function updateImageSource(size, facade) {
+    const combinationKey = size + '_' + facade;
+    const imageSources = {
+      '1_1': 'https://rodam-home.dk/wp-content/uploads/2022/01/design-dit-eget-anneks-26-m2.jpg',
+      '2_1': 'https://rodam-home.dk/wp-content/uploads/2022/01/anneks-30-40-m2-RODAM-33m2.jpg',
+      '3_1': 'https://rodam-home.dk/wp-content/uploads/2022/01/Anneks-rodam-home-03-38-m2-ude.jpg',
+      '4_1': 'https://rodam-home.dk/wp-content/uploads/2022/01/design-dit-eget-anneks-44-m2.jpg',
+      '1_2': 'https://rodam-home.dk/wp-content/uploads/2022/01/anneks-20-30-m2-RODAM-26m2-sort-trae.jpg',
+      '2_2': 'https://rodam-home.dk/wp-content/uploads/2022/01/anneks-30-40-m2-RODAM-33m2-sort.jpg',
+      '3_2': 'https://rodam-home.dk/wp-content/uploads/2022/01/Anneks-rodam-home-03-38-m2-sort.jpg',
+      '4_2': 'https://rodam-home.dk/wp-content/uploads/2022/01/anneks-40-50-m2-RODAM-44-m2-sort.jpg'
+    };
+
+    const currentImageSource = imageSources[combinationKey] || 'https://rodam-home.dk/wp-content/uploads/2022/01/design-dit-eget-anneks-26-m2.jpg';
+    combinationImage.src = currentImageSource;
+  }
+});
+
+
+
+
+
+
+// Dette er kun til #001
+document.addEventListener('DOMContentLoaded', function () {
+  const price1 = 40000;
+  const price2 = 40000;
+  const price3 = 20000;
+
+  const price1Cell = document.getElementById('#1price1');
+  const price2Cell = document.getElementById('#1price2');
+  const price3Cell = document.getElementById('#1price3');
+  const totalPriceCell = document.getElementById('#1totalPrice');
+
+  price1Cell.textContent = price1;
+  price2Cell.textContent = price2;
+  price3Cell.textContent = price3;
+
+  const totalPrice = price1 + price2 + price3;
+  totalPriceCell.textContent = totalPrice;
+});
+
+
+
+
+
+// Dette er kun til #002
+document.addEventListener('DOMContentLoaded', function () {
+  const areaPrice = 10000;
+  
+
+
+
+
+
+
+
 
